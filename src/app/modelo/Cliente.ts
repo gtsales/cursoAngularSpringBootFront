4c0588c24607b092cfe9295a9506d5dabf94c6f2
gtsales <soutoluiz0609@gmail.com>
2023-08-15 12:20:09 -0300

    Classe Cliente criada no front

---
+++ src/app/modelo/Cliente.ts
@@ -0,0 +1,8 @@
+export class Cliente{//Declara nossa entidade no front, para que seja possível manipular essa informações no back
+
+    codigo:number = 0;
+    nome:string = '';
+    idade:number = 0;
+    cidade:string = '';
+
+}

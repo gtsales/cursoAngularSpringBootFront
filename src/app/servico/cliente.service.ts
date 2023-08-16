b422dd4d48e0d27d070506257ea06eb94ad66602
gtsales <soutoluiz0609@gmail.com>
2023-08-16 11:07:56 -0300

    Inicio da configuração para requisição via front

--- src/app/servico/cliente.service.ts
+++ src/app/servico/cliente.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from '../modelo/Cliente';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +12,12 @@ export class ClienteService {
   private url:string = 'http://localhost:8080';
 
   constructor(private http:HttpClient) { }//declara o http um objeto da classe HttpClient, para através dele realizar requisições http
+
+  //Método para selecionar todos os clientes
+  selecionar():Observable<Cliente[]>{
+
+    return this.http.get<Cliente[]>(this.url);
+
+  }
 
 }
